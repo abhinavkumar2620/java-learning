303d0e66b50286e43229a9a272988ffd5ace8d86
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-03-26 14:09:55 +0530

    my first nptl

---
+++ first.java
@@ -0,0 +1,21 @@
+public class first {
+      public static void main(String[] args) {
+        
+      int a= 10;
+      int b=50;
+      int sum = a+b;
+      System.out.println(sum);
+      
+      
+      int mul = a * b;
+      System.out.println(mul);
+
+
+
+
+
+
+      }
+
+
+}
