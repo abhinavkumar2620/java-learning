7ce3df508767f332a8e43b2b95364d46ca0c7db5
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-04-26 23:09:18 +0530

    type conversion and type casting

---
+++ casting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class casting {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double a = 1.2548;
+    int b = (int) a;
+    System.out.println(b);
+    int c = 5;
+    double d =(double) c;
+    System.out.println(d);
+  }
+}
