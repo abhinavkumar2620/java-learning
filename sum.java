03fca194b6b3bb578f6e97b584acb793e64b49b4
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-04-26 18:13:44 +0530

    Added basic Java programs - loops and patterns

--- sum.java
+++ sum.java
@@ -1,8 +1,12 @@
+import java.nio.channels.ScatteringByteChannel;
+import java.util.Scanner;
+// sum of two number by taking user input
 public class sum {
   public static void main(String[] args) {
-  
-    int a = 10; 
-    int b = 5;
+  Scanner sc = new Scanner(System.in);
+    
+     int a = sc.nextInt ();
+    int b = sc.nextInt ();
     int sum = a + b; // sum code
     System.out.println(sum);
   }
