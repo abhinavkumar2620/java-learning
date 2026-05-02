0cd862407a981cd700418fa8fe4079f249f95d05
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-05-02 13:32:59 +0530

    printing hello world for 100 times by using while loop

---
+++ 06-loops/HelloWorld100Times.java
@@ -0,0 +1,11 @@
+public class HelloWorld100Times {
+// made by me  , by using while loop 
+  public static void main(String[] args) {
+    int counter = 0;
+    while (counter < 100) {
+      System.out.println("Hello World");
+      counter++;
+    }
+    System.out.println("it is printed for 100 times");
+  }
+}
