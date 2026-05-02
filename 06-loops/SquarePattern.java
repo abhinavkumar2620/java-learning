6a432a1e40963888a627b7bbb4bc6e758cf4ca02
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-05-02 16:05:50 +0530

    this program prints a square pattern of asterisks using for loop

---
+++ 06-loops/SquarePattern.java
@@ -0,0 +1,9 @@
+public class SquarePattern {
+  // this program prints a square pattern of asterisks using for loop
+  public static void main(String[] args) {
+    for (int line = 1; line <=4; line++){
+      System.out.println("****");
+    }
+    System.out.println("done");
+  }
+}
