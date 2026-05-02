4da6bb3dfd4b527a06b32292b5617a0901befb6b
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-05-02 13:53:36 +0530

    This program is going to print number form 1 to 10 by using while loop

---
+++ 06-loops/NumberFrom1To10.java
@@ -0,0 +1,11 @@
+public class NumberFrom1To10 {
+  // This program is going to print number form 1 to 10 by using while loop
+  public static void main (String [] args){
+    int i = 1;
+    while (i<=10){
+    System.out.println(i);
+    i++;
+    }
+    System.out.println("Done!");
+  }
+}
