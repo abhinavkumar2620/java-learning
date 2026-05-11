c59ea90fc8f8bf6b673998cae6d9bce407545956
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-05-11 23:01:24 +0530

    This program calculates the GCD (Greatest Common Divisor) of two given numbers

---
+++ 06-loops/GCDNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+// This program calculates the GCD (Greatest Common Divisor) of two given numbers
+public class GCDNumber {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter first number: ");
+    int a = sc.nextInt();
+
+    System.out.print("Enter second number: ");
+    int b = sc.nextInt();
+
+    int gcd = 1;
+
+    for (int i = 1; i <= a && i <= b; i++) {
+
+      if (a % i == 0 && b % i == 0) {
+        gcd = i;
+      }
+    }
+
+    System.out.println("GCD = " + gcd);
+
+    sc.close();
+  }
+}
