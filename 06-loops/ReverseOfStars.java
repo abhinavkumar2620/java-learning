ce801c1f17c97245e38394a3bdff949efa21048d
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-05-12 23:04:12 +0530

    This program prints a right-angled triangle of stars in reverse order using nested loops

---
+++ 06-loops/ReverseOfStars.java
@@ -0,0 +1,14 @@
+public class ReverseOfStars {
+  // This program prints a right-angled triangle of stars in reverse order using nested loops
+  public static void main(String[] args) {
+
+    for (int line = 4; line >= 1; line--) {
+
+      for (int star = 1; star <= line; star++) {
+        System.out.print("*");
+      }
+
+      System.out.println();
+    }
+  }
+}
