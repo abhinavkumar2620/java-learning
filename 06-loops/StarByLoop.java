2e5f7cdb1b4c8bcf243030c6a35e4dca3da1bbb3
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-05-12 23:01:46 +0530

    This program prints a right-angled triangle of stars using nested loops

--- 06-loops/StarByLoop.java
+++ 06-loops/StarByLoop.java
@@ -1,7 +1,7 @@
 public class StarByLoop {
   // This program prints a right-angled triangle of stars using nested loops
 public static void main(String[] args) {
-   for(int line = 1; line <=4; line++)
+  for(int line = 1; line <=4; line++)
         {
             for (int star = 1; star <=line;star++){
             System.out.print("*");
