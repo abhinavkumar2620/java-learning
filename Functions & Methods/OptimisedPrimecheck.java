61d36746c2b25bcbafdf8841ac47ae076d22a62f
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-05-26 19:38:21 +0530

    this program checks if a number is prime or not using an optimised method that checks for factors only up to the square root of the number

---
+++ Functions & Methods/OptimisedPrimecheck.java
@@ -0,0 +1,15 @@
+//this program checks if a number is prime or not using an optimised method that checks for factors only up to the square root of the number.
+public class OptimisedPrimecheck {
+  public static boolean isprime(int n){
+  for(int i = 2; i<=Math.sqrt(n); i++){
+    if (n%i==0){
+      return false;
+    }
+  }
+  return true;
+  }
+  public static void main(String[]args){
+  int n=28;
+  System.out.println(isprime(n));
+  }
+}
