8f30aadb30e0804a486585439979c0f1a81646a5
abhinavkumar4535-prog <abhinavkumar4535@gmail.com>
2026-06-18 01:11:34 +0530

    This program implements the binary search algorithm to find the index of a given key in a sorted array.

---
+++ 08-array/Binary.java
@@ -0,0 +1,31 @@
+public class Binary {
+// This program implements the binary search algorithm to find the index of a given key in a sorted array. If the key is found, it returns the index; otherwise, it returns -1.
+  public static int binary(int number[], int key) {
+
+    int start = 0;
+    int end = number.length - 1;
+
+    while (start <= end) {
+
+      int mid = (start + end) / 2;
+      if (number[mid] == key) {
+        return mid;
+      }
+
+      if (number[mid] > key) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+
+    return -1;
+  }
+
+  public static void main(String[] args) {
+
+    int number[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int key = 9;
+    System.out.println("The index value is " + binary(number, key));
+  }
+}
